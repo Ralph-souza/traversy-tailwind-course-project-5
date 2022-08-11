3413ae90fa59d065cfb13805669cb27022c261ef
Ralph <ralph_souza@hotmail.com>
2022-08-11 20:19:18 -0300

    Adiciona configuracoes iniciais para producao

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: '480px',
@@ -8,7 +9,33 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)'
+      },
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif'],
+        opensans: ['Opensans', 'sans-serif']
+      },
+      backgroundImage: (theme) => ({
+        'logo-dark-mode': "url('../images/logo-dark-mode.svg')",
+        'logo-light-mode': "url('../images/logo-light-mode.svg')",
+        'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')"
+      })
+    },
   },
+  variants: {
+    extend: {
+      backgroundImage: ['dark']
+    }
+  },
+
   plugins: [],
 }
