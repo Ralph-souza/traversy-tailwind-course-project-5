85217abff6de76b3420bb027654d782d44bfc90c
Ralph <ralph_souza@hotmail.com>
2022-08-15 09:41:51 -0300

    Adiciona a hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,13 +27,13 @@ module.exports = {
         'logo-dark-mode': "url('../images/logo-dark-mode.svg')",
         'logo-light-mode': "url('../images/logo-light-mode.svg')",
         'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
-        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')"
-      })
+        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')",
+      }),
     },
   },
   variants: {
     extend: {
-      backgroundImage: ['dark']
+      backgroundImage: ['dark'],
     }
   },
 
